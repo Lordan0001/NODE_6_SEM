b49b301de7ffb5d0c1c8ed61db9b65dfad00b77b
Vladislav <4belstu@mail.ru>
2023-05-09 13:44:28 +0300

    changed to from callback to async await

--- WriteIt/controllers/PostController.js
+++ WriteIt/controllers/PostController.js
@@ -15,37 +15,27 @@ export const getOne = async (req, res) => {
     try {
         const postId = req.params.id;
 
-        PostModel.findOneAndUpdate(
-            {
-            _id: postId
-        },
-            {
-                $inc: { viewsCount: 1 },
-            },
-            {
-                returnDocument: 'after',
-            },
-            (err, doc) =>{
-            if(err){
-                    console.log(err);
-                    return res.status(500).json({
-                        message: 'Failed to return post'
-                    })
-                }
-            if(!doc){
-                return res.status(404).json({
-                    message: 'One post not found'
-                })
-            }
-                res.json(doc);
-            });
+        const doc = await PostModel.findOneAndUpdate(
+            { _id: postId },
+            { $inc: { viewsCount: 1 } },
+            { returnDocument: 'after' }
+        );
+
+        if (!doc) {
+            return res.status(404).json({
+                message: 'One post not found'
+            })
+        }
+
+        res.json(doc);
     } catch (err) {
         console.log(err);
         res.status(500).json({
-            message: 'posts not found'
+            message: 'Failed to return post'
         })
     }
 }
+
 
 export const create = async (req, res) => {
     try{
