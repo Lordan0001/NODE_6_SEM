7710c758933a08c0714a07ec4a5d6711d7dcfd1d
Vladislav <4belstu@mail.ru>
2023-09-20 19:12:41 +0300

    delete trash

--- new/25/25/routers/abilityRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express');
-const abilityRouter = new Router();
-const abilityController = require('../controllers/abilityController');
-
-abilityRouter
-    .get('/', abilityController.getAllRules);
-
-module.exports = abilityRouter;
--- new/rm/lab9/routers/user.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {getAll, getUser, getAbility} = require('../controllers/user');
-const {auth} = require('../middleware/index');
-
-router.get('/ability', getAbility);
-router.get('/', auth, getAll);
-router.get('/:id', auth, getUser);
-
-module.exports = router;
